e047440bdc48cad03deb37d74e5da2af53bb34d4
Natasha Kinuthia <kinuthianatasha@gmail.com>
2021-05-26 10:23:53 +0300

    updated models.py

--- reddit/models.py
+++ reddit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.conf import settings
 
 class BaseModel(models.Model):
     eid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
